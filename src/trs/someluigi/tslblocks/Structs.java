7033e56454dc28a04aa9271039fd9901b5c56f47
TehSomeLuigi <olivier.the.olive@gmail.com>
2013-08-20 14:52:14 +0100

    Colour lookup table

---
+++ src/trs/someluigi/tslblocks/Structs.java
@@ -0,0 +1,24 @@
+package trs.someluigi.tslblocks;
+
+public class Structs {
+	public static String[] colours = new String[16];
+	
+	static {
+		colours[0] = "White";
+		colours[1] = "Orange";
+		colours[2] = "Magenta";
+		colours[3] = "Light Blue";
+		colours[4] = "Yellow";
+		colours[5] = "Lime";
+		colours[6] = "Pink";
+		colours[7] = "Gray";
+		colours[8] = "Light Gray";
+		colours[9] = "Cyan";
+		colours[10] = "Purple";
+		colours[11] = "Blue";
+		colours[12] = "Brown";
+		colours[13] = "Green";
+		colours[14] = "Red";
+		colours[15] = "Black";
+	}
+}
